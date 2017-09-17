3d6777b44830c20971bda62cae3fc35e74678f8f
Leif Anderson <leif.v.anderson@gmail.com>
2017-09-16 21:48:18 -0500

    barcode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Retro Ninja Switchboard';
+  colors = [
+    {'background-color':'red'},
+    {'background-color':'green'},
+    {'background-color':'blue'},
+    {'background-color':'yellow'},
+    {'background-color':'cyan'},
+    {'background-color':'black'},
+    {'background-color':'white'}];
+
+  getRandomColor() {
+    return this.colors[Math.floor(Math.random()*6)+1];
+  }
 }
